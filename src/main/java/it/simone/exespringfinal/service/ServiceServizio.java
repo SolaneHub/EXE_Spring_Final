804763c8df9e2303b28e8775925467c996bc5832
SimoMobile2 <simonelostimolo@gmail.com>
2025-07-27 20:08:58 +0200

    Sesto Commit

--- src/main/java/it/simone/exespringfinal/service/ServiceServizio.java
+++ src/main/java/it/simone/exespringfinal/service/ServiceServizio.java
@@ -5,24 +5,44 @@ import org.springframework.stereotype.Service;
 
 import it.simone.exespringfinal.entity.Servizio;
 import it.simone.exespringfinal.repository.RepositoryServizio;
+import it.simone.exespringfinal.repository.RepositoryTipoServizio;
 
 @Service
 public class ServiceServizio extends AbstractService<Servizio, Long> {
 
-	public final RepositoryServizio repository;
+	public final RepositoryServizio repositoryServizio;
+	public final RepositoryTipoServizio repositoryTipoServizio;
 
-	public ServiceServizio(RepositoryServizio repository) {
-		this.repository = repository;
+	public ServiceServizio(RepositoryServizio repositoryServizio, RepositoryTipoServizio repositoryTipoServizio) {
+		this.repositoryServizio = repositoryServizio;
+		this.repositoryTipoServizio = repositoryTipoServizio;
 	}
 
 	@Override
 	protected JpaRepository<Servizio, Long> getRepository() {
-		return repository;
+		return repositoryServizio;
 	}
 
 	@Override
 	protected String getEntityName() {
 		return "Servizio";
+	}
+
+	@Override
+	public Servizio save(Servizio entity) {
+		if (entity.getTipoServizio() != null) {
+			if (entity.getTipoServizio().getId() == null) {
+				var nuovoTipoServizio = repositoryTipoServizio.save(entity.getTipoServizio());
+				entity.setTipoServizio(nuovoTipoServizio);
+			} else {
+				var tipoServizioEsistente = repositoryTipoServizio.findById(entity.getTipoServizio().getId())
+						.orElseThrow(() -> new RuntimeException(
+								"TipoServizio con id " + entity.getTipoServizio().getId() + " non trovato"));
+				entity.setTipoServizio(tipoServizioEsistente);
+			}
+		}
+
+		return super.save(entity);
 	}
 
 	public Servizio updateById(Long id, Servizio entityDetails) {
@@ -32,6 +52,20 @@ public class ServiceServizio extends AbstractService<Servizio, Long> {
 			servizio.setDescrizione(entityDetails.getDescrizione());
 			return save(servizio);
 		}).orElseThrow(() -> new RuntimeException("Servizio con id: " + id + " non trovato"));
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		Servizio servizio = repositoryServizio.findById(id)
+				.orElseThrow(() -> new RuntimeException("Servizio con id: " + id + " non trovato"));
+
+		var tipoServizio = servizio.getTipoServizio();
+
+		super.deleteById(id);
+
+		if (tipoServizio != null) {
+			repositoryTipoServizio.delete(tipoServizio);
+		}
 	}
 
 }
