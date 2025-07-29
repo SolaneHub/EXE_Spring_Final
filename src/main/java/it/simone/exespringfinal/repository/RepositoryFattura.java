71857d71469b250ece3e50a10a24c7cced44824c
SimoMobile2 <simonelostimolo@gmail.com>
2025-07-29 22:53:03 +0200

    Decimo Commit

--- src/main/java/it/simone/exespringfinal/repository/RepositoryFattura.java
+++ src/main/java/it/simone/exespringfinal/repository/RepositoryFattura.java
@@ -1,9 +1,16 @@
 package it.simone.exespringfinal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import it.simone.exespringfinal.entity.Fattura;
+import it.simone.exespringfinal.entity.StatoFattura;
 
 public interface RepositoryFattura extends JpaRepository<Fattura, Long> {
+
+	List<Fattura> findByClienteRagioneSociale(String ragioneSociale);
+
+	List<Fattura> findByStato(StatoFattura stato);
 
 }
--- src/main/java/it/simone/exespringfinal/service/ServiceFattura.java
+++ src/main/java/it/simone/exespringfinal/service/ServiceFattura.java
@@ -1,23 +1,26 @@
 package it.simone.exespringfinal.service;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import it.simone.exespringfinal.entity.Fattura;
+import it.simone.exespringfinal.entity.StatoFattura;
 import it.simone.exespringfinal.repository.RepositoryFattura;
 
 @Service
 public class ServiceFattura extends AbstractService<Fattura, Long> {
 
-	private final RepositoryFattura repository;
+	private final RepositoryFattura repositoryFattura;
 
-	public ServiceFattura(RepositoryFattura repository) {
-		this.repository = repository;
+	public ServiceFattura(RepositoryFattura repositoryFattura) {
+		this.repositoryFattura = repositoryFattura;
 	}
 
 	@Override
 	protected JpaRepository<Fattura, Long> getRepository() {
-		return repository;
+		return repositoryFattura;
 	}
 
 	@Override
@@ -34,6 +37,14 @@ public class ServiceFattura extends AbstractService<Fattura, Long> {
 			fattura.setCliente(entityDetails.getCliente());
 			return save(fattura);
 		}).orElseThrow(() -> new RuntimeException("Fattura con id: " + id + " non trovato"));
+	}
+
+	public List<Fattura> findByClienteRagioneSociale(String ragioneSociale) {
+		return repositoryFattura.findByClienteRagioneSociale(ragioneSociale);
+	}
+
+	public List<Fattura> findByStato(StatoFattura stato){
+		return repositoryFattura.findByStato(null)
 	}
 
 }
